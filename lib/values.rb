5d2925b74f46fed1e868ff048cf2ee759ad74e5e
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-15 19:48:25 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -8,6 +8,10 @@ require 'pry'
 #  grains: ["rice", "pasta"]
 # }
 
-def get_the_values(groceries)
-  #code your solution here!
+def get_the_values (groceries.keys) {dairy: ["milk","yogurt","cheese"],
+  vegetable: ["carrots","broccoli","cucumbers"],
+  meat: ["chicken","steak","salmon"],
+  grains: ["rice","pasta"]
+}
+
 end
